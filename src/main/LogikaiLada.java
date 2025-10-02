44e6174fc571afd0f745be70c873f1957ebd201a
Eszti2222 <nagy.eszter2222@gmail.com>
2025-10-02 15:23:44 +0200

    LadaModell

--- src/main/LogikaiLada.java
+++ src/main/LogikaiLada.java
@@ -2,8 +2,34 @@ package main;
 
 public class LogikaiLada {
 
-    public static void main(String[] args) {
+    public Mvc02KonzolEmber() {
+        //CuiNezet nezet = new CuiNezet();
+        //JopNezet nezet = new JopNezet();
+
+        //EmberModell modell = new EmberModell(nezet.getNev(), nezet.getKor());
+        EmberModell modell = new EmberModell();
         
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                //GuiNezet3 nezet = new GuiNezet3();
+                /* itt is lehet, de inkább a vezérlő tegye*/
+                //nezet.setVisible(true);
+                //GuiVezerlo3 vezerlo = new GuiVezerlo3(modell, nezet);
+                //vezerlo.feladat();
+                
+                GuiNezet4 nezet = new GuiNezet4();
+                GuiVezerlo4 vezerlo = new GuiVezerlo4(modell, nezet);
+                vezerlo.feladat();
+            }
+        });
+        
+        //CuiVezerlo vezerlo = new CuiVezerlo(modell, nezet);
+        //JopVezerlo vezerlo = new JopVezerlo(modell, nezet);
+        //vezerlo.feladat();
+    }
+
+    public static void main(String[] args) {
+        new Mvc02KonzolEmber();
     }
     
 }
---
+++ src/modell/LadaModell.java
@@ -0,0 +1,15 @@
+package modell;
+
+public class LadaModell {
+
+    private String lada;
+
+    public LadaModell(String lada) {
+        this.lada = lada;
+    }
+
+    public String getLada() {
+        return lada;
+    }
+
+}
