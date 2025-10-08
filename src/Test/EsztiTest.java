28571df838e5001764cf26113e09427c7e1b7633
Nagy Eszter Erzsébet (Szf_N_2024_2026) <nagy.eszter.erzsebet@diak.szamalk-szalezi.hu>
2025-10-08 15:13:35 +0200

    Eszti_test_a kivalasztott ladara megfelelo visszajelzest kapunk (benne a kincs, nincs benne)

--- src/Test/EsztiTest.java
+++ src/Test/EsztiTest.java
@@ -1,6 +1,19 @@
-
+//a kiválasztott ládára megfelelő visszajelzést kapunk (benne a kincs, nincs benne)
 package Test;
+
+import modell.LadaModell;
+import nezet.GUINezet;
+import vezerlo.GUIVezerlo;
 
 public class EsztiTest {
     
+
+    public static void main(String[] args) {
+        // Tesztelés engedélyezése: java -ea GUIVezerloTest
+
+        GUINezet nezet = new GUINezet(); // valódi nézet
+        LadaModell modell = new LadaModell();
+        GUIVezerlo vezerlo = new GUIVezerlo(modell, nezet);
+        vezerlo.feladat();
+
 }
