4323b9e4b29b0a4368e74ad2e87b2a0f4051623d
NádasViktóriaMária(S <NdasViktriaMria(S@192.168.228.8>
2025-10-08 15:10:27 +0200

    viki test eleje/alap

--- src/Test/VikiTest.java
+++ src/Test/VikiTest.java
@@ -1,5 +1,17 @@
 package Test;
 
+import modell.LadaModell;
+import nezet.GUINezet;
+import vezerlo.GUIVezerlo;
+
 public class VikiTest {
+    //vezérlő 2:
+    //a visszajelzés szövege megfelelő ("Gratulálunk...", "Sajnos  nem nyert...")
     
+    public static void main(String[] args) {
+        void testFeladat(){
+            GUIVezerlo obj = new GUIVezerlo( modell,  nezet);
+        }
+        obj.feladat();
+    }
 }
